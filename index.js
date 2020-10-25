e8c4a8a0f2d8eadda8618df7fb40baa15e8363d7
jcpv31298 <70558204+jcpv31298@users.noreply.github.com>
2020-10-25 13:17:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,45 +10,62 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'clase-node'
+    database: 'celulares'
 });
 
 // Start connection to database
 connection.connect();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hola mundo soy Carlos Paez");
 });
 
-app.get("/personajes", (req, res) => {
-    connection.query('SELECT * FROM personajes', function(error, results, fields) {
+app.get("/celular", (req, res) => {
+    connection.query('SELECT * FROM celular', function(error, results, fields) {
         if (error) {
-            return res.status(400).json({ "error": "Consulta no valida" });
+            return res.status(400).json({ error: "Consulta no valida" });
         }
         res.status(200).json(results);
     });
 });
 
-app.get("/personajes/:id", (req, res) => {
+app.get("/celular/:id", (req, res) => {
 
     const id = req.params.id;
 
     if (isNaN(id)) {
-        return res.status(400).json({ "error": "Parametros no validos" });
+        return res.status(400).json({ error: "Parametros no validos" });
     }
 
-    connection.query(`SELECT * FROM personajes WHERE id = ?`, [id], function(error, results, fields) {
+    connection.query(`SELECT * FROM celular WHERE id = ?`, [id], function(error, results, fields) {
         if (error) {
-            return res.status(400).json({ "error": "Consulta no valida" });
+            return res.status(400).json({ error: "Consulta no valida" });
         }
 
         if (results.length === 0) {
-            return res.status(400).json({ "error": `El personaje con el id ${id} no existe` });
+            return res.status(400).json({ error: `El celular con el id ${id} no existe` });
         }
 
         res.status(200).json(results);
+    });
+});
+
+app.post("/celular", (req, res) => {
+
+    const marca = req.body.marca;
+    const modelo = req.body.modelo;
+    const descripcion = req.body.descripcion;
+    const precio = req.body.precio;
+
+    connection.query('INSERT INTO celular(marca,modelo,descripcion,precio) VALUES(?,?,?,?)', [marca, modelo, descripcion, precio], function(error, results, fields) {
+        if (error) {
+            return res.status(400).json({ "error": "Consulta no valida" });
+        }
+        res.status(200).json({ success: "Registro insertado correctamente." });
     });
 });
 
